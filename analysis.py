dbd144c98599f3d86dd6fb38e79c536964ee35b8
Ben Goldstein <27benjamin.goldstein@gmail.com>
2021-12-02 13:42:53 -0500

    store valid article types in constant

--- analysis.py
+++ analysis.py
@@ -29,6 +29,7 @@ START_DECADE = 1660
 END_DECADE = 1920
 DECADES = list(range(START_DECADE, END_DECADE + 1, 10))
 NUM_DECADES = int((END_DECADE - START_DECADE)/10) + 1
+ARTICLE_TEXT_TYPES = ["fla", "article"]
 
 #PARAMETER CONSTANTS
 COMMON_WORDS = ["a", "able", "about", "across", "after", "all", "almost", "also", "am", "among", "an", "and" , "any", "are", "as", "at", "be", "because", "been", "but", "by", "can", "cannot", "could", "dear", "did", "do", "does", "either", "else", "ever", "every", "for", "from", "get", "got", "had", "has", "have", "he", "her", "hers", "him", "his", "how", "however", "i", "if", "in" , "into", "is" , "it", "its", "just", "least", "let", "like", "likely", "may", "me", "might", "most", "must", "my", "neither", "no", "nor", "not", "of", "off", "often", "on", "only", "or" , "other", "our", "own", "rather", "said", "say", "says", "she", "should", "since", "so", "some", "than", "that", "the", "their", "them", "then", "there", "these", "they", "this", "tis", "to", "too", "twas", "us", "wants", "was", "we", "were", "what", "when", "where", "which", "while", "who", "whom", "why", "will", "with", "would", "yet", "you", "your"]
@@ -79,7 +80,7 @@ def perform_topic_analysis():
 					ind += 1
 				header_row = False
 
-			elif row[index_table[TYPE_COL]] == "fla" or row[index_table[TYPE_COL]] == "article":
+			elif row[index_table[TYPE_COL]] in ARTICLE_TEXT_TYPES:
 				# get relevant info from row
 				primary_topic = row[index_table[PRIMARY_TOPIC_COL]]
 				secondary_topic = row[index_table[SECONDARY_TOPIC_COL]]
@@ -246,7 +247,7 @@ def perform_text_analysis():
 					ind += 1
 				header_row = False
 
-			elif row[index_table[TYPE_COL]] == "fla" or row[index_table[TYPE_COL]] == "article":
+			elif row[index_table[TYPE_COL]] in ARTICLE_TEXT_TYPES:
 				text_key = (row[index_table[PRIMARY_TOPIC_COL]], int(row[index_table[DECADE_COL]]))
 				if not (row[index_table[PRIMARY_TOPIC_COL]] in primary_topics):
 					primary_topics.append(row[index_table[PRIMARY_TOPIC_COL]])
